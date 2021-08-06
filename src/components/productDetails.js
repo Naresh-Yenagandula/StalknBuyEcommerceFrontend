e9a2fe20a71454c7b0e5bf544124d569f83c1f14
Tejas Borkar <tvborkar1@gmail.com>
2021-08-06 12:00:27 +0530

    just

--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -10,10 +10,10 @@ function ProductDetails() {
 
     useEffect(() => {
         if (!value.FilterProducts) {
-            axios.get(`${process.env.REACT_APP_BACKEND_URL}/product/id`)
+            axios.get(`${process.env.REACT_APP_BACKEND_URL}/product/${id}`)
                 .then((res) => {
-                    setidData(res.data.products)
-                    console.log(res.data.products);
+                    setidData(res.data.products[0])
+                    console.log(res.data.products[0]);
                 })
                 .catch((err) => {
                     console.log(err);
@@ -25,6 +25,34 @@ function ProductDetails() {
     }, [])
     return (
         <div>
+             <div className="container-fluid">
+                            <div className="row">
+                                {idData?
+                                
+                                <>
+                                
+                                <div className="col-md-4 offset-md-1 col-sm-6">
+                                    <img className="img-fluid modal-image"  src={idData.IMAGE}/>
+                                </div>
+                                <div className="col-md-6 col-sm-6 offset-md-1 text-center mt-5">
+                                 <h3>{idData.BRAND}</h3>
+                                 {/* <p>{value.extractData(idData).desc}</p>     */}
+                                 <h5>Rs. {idData.PRICE}</h5>    
+                                 <p className="mt-5"> Select size</p>  
+                                 <div>
+                                     <button className="btn btn-secondary rounded-circle size-button">S</button>
+                                     <button className="btn btn-secondary rounded-circle size-button">M</button>
+                                     <button className="btn btn-secondary rounded-circle size-button">L</button>
+                                     <button className="btn btn-secondary rounded-circle size-button">XL</button>
+                                     {/* <button className="btn btn-secondary rounded-circle size-button">XXL</button> */}
+                                 </div>    
+
+                                 <button className="btn bag-button mt-5">ADD TO BAG</button>   
+                                 
+                                </div></>:null}
+
+                            </div>
+                            </div>
 
         </div>
     )
