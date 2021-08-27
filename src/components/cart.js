43ae401bc506f6b5b40d7b0411713d3da710e86e
Namrata <namratarana9@gmail.com>
2021-08-27 22:58:48 +0530

    100

--- src/components/cart.js
+++ src/components/cart.js
@@ -237,7 +237,29 @@ function Carts(props) {
 
                                         </div> :
                                         <div>
-                                            hello
+                                           <div className="my-4 ps-3"> <b>Choose Payment Mode</b> </div>
+                                            <div className="border m-2 p-3">
+                                                <div class="d-flex align-items-start ">
+                                                    <div class="nav flex-column nav-pills bg-light w-25" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                                                        <button class=" nav-link text-start " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true"><small style={{fontSize:'12px'}}> PHONEPE/GOOGLE PAY/AMAZON PAY</small></button>
+                                                        <button class=" nav-link text-start" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><small style={{fontSize:'12px'}}>CASH ON DELIVERY (CARD/CASH/UPI)</small> </button>
+                                                        <button class="nav-link text-start" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false"><small style={{fontSize:'12px'}}>CREDIT CARD/DEBIT CARD</small></button>
+                                                        <button class="nav-link text-start" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><small style={{fontSize:'12px'}}>WALLETS</small></button>
+                                                        <button class="nav-link text-start" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><small style={{fontSize:'12px'}}>NET BANKING</small></button>
+                                                        <button class="nav-link text-start" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false"><small style={{fontSize:'12px'}}>EMI</small></button>
+
+                                                    </div>
+                                                    <div class="tab-content" id="v-pills-tabContent">
+                                                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                                                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                                                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                                                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                                                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                                                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>}
                             </div >
 
